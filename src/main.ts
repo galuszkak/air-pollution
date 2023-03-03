35f5e0beda018aa5b987800016d66aefc9e11e4e
Kamil Gałuszka <kamil@flyrlabs.com>
2023-03-03 12:38:36 +0100

    fix: small updates

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService)
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   console.log("PORT: " + configService.get('PORT'))
   await app.listen(configService.get('PORT'));
 }
